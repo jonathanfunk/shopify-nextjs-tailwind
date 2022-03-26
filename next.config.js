0a9f562f0c58b05f45be46763ef3b2c6f934d89b
Jonathan Funk <jonfunk88@hotmail.com>
2022-03-26 09:26:50 -0700

    added env configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
+    SHOPIFY_STOREFRONT_ACCESSTOKEN: process.env.SHOPIFY_STOREFRONT_ACCESSTOKEN,
+  },
+  images: {
+    domains: ['cdn.shopify.com'],
+  },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
