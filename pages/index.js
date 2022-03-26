6f95cffbc9c0ab80f5681abe8294b10692e6eaf4
Jonathan Funk <jonfunk88@hotmail.com>
2022-03-26 11:07:59 -0700

    created Shopify product query

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import { getProductsInCollection } from '../lib/shopify';
 
-export default function Home() {
+export default function Home({ products }) {
+  console.log(products);
   return <h1 className='text-3xl'>Hello world!</h1>;
+}
+
+export async function getStaticProps(context) {
+  const products = await getProductsInCollection();
+  return {
+    props: { products },
+  };
 }
